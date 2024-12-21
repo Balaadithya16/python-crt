a3d9541d5930de88fef21219a7fc5f022ce744d3
V.BalaAdithya <vacchaadithya@gmail.com>
2024-12-21 10:03:50 +0530

    Update stack.py

--- stack.py
+++ stack.py
@@ -1,11 +1,12 @@
 def push():
     a=int(input("Enter push element:"))
-    l.append(a)
+    l.insert(0,a)
 def pop():
     if len(l)==0:
         print("Stack is empty")
     else:
-        print("The pop element is",l.pop())
+        print("The pop element is",l[0])
+        l.remove(l[0])
 def exit():
     return False
 l=[] 
